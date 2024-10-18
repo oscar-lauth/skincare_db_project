d746ed66b112bf158d475c94f3ae2c403299245f
Kaitlyn <90528910+ksong02@users.noreply.github.com>
2024-10-18 01:08:47 -0400

    Update contains.sql

--- P3/contains.sql
+++ P3/contains.sql
@@ -1,1 +1,10 @@
+USE P3
+GO
 
+CREATE TABLE Contains (
+    productID INT NOT NULL REFERENCES Product(productID),
+    ingredientID INT NOT NULL REFERENCES Ingredient(ingredientID),
+    PRIMARY KEY (productID, ingredientID)
+);
+
+INSERT INTO Contains (productID, ingredientID) VALUES
