ee0eb23461b60080e17f7983905c4ad53f12d3cd
emmittjames <emmittjames1@gmail.com>
2024-10-18 01:29:01 -0400

    added join queries

--- P3/queries.sql
+++ P3/queries.sql
@@ -16,3 +16,16 @@ FROM Sunscreen;
 SELECT skinType, COUNT(userID) AS totalUsers
 FROM Users
 GROUP BY skinType;
+
+-- find all products that are part of a user’s routine along with the routine’s details
+SELECT p.productName, r.summary, r.timeOfDay
+FROM Product p
+JOIN Has h ON p.productID = h.productID
+JOIN Routine r ON h.routineID = r.routineID;
+
+-- list all cleansers and their details that are included in user routines
+SELECT p.productName, c.cleanserForm, c.removesMakeup, r.summary
+FROM Cleanser c
+JOIN Product p ON c.productID = p.productID
+JOIN Has h ON p.productID = h.productID
+JOIN Routine r ON h.routineID = r.routineID;
