07e260b3c625936ffdecd28b19897f5ae06ee8e6
emmittjames <emmittjames1@gmail.com>
2024-10-18 01:59:06 -0400

    added order to execute sql files in

--- P3/init_db.sql
+++ P3/init_db.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE P3;
+
+-- order of execution for SQL files:
+-- 1. product.sql
+-- 2. users.sql
+-- 3. ingredient.sql
+-- 4. cleanser.sql
+-- 5. moisturizer.sql
+-- 6. sunscreen.sql
+-- 7. ingredient_category.sql
+-- 8. routine.sql
+-- 9. reviews.sql
+-- 10. conflicts_with.sql
+-- 11. includes.sql
+-- 12. has.sql
+-- 13. favorites.sql
