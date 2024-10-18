8a06580812b73fbb6685808af5744136ea337b4e
Kaitlyn <90528910+ksong02@users.noreply.github.com>
2024-10-18 00:54:34 -0400

    Update and rename incompatibility to ConflictsWith.sql

---
+++ P3/ConflictsWith.sql
@@ -0,0 +1,56 @@
+
+USE P3
+GO
+
+CREATE TABLE ConflictsWith(
+  ingredientID1 INT FOREIGN KEY REFERENCES IngredientCategory(categoryID)
+  ingredientID2 INT FOREIGN KEY REFERENCES IngredientCategory(categoryID)
+);
+
+
+INSERT INTO ConflictsWith (ingredientID1, ingredientID2) VALUES
+(1, 3),
+(1, 5),
+(1, 6),
+(1, 13),
+(1, 19),
+(1, 21),
+(2, 3),
+(2, 5),
+(2, 6),
+(2, 13),
+(2, 19),
+(2, 21),
+(3, 1),
+(3, 2),
+(3, 6),
+(3, 13),
+(3, 21),
+(5, 1),
+(5, 2),
+(5, 6),
+(5, 15),
+(6, 1),
+(6, 2),
+(6, 3),
+(6, 5),
+(6, 12),
+(6, 13),
+(6, 19),
+(9, 21),
+(12, 6),
+(13, 1),
+(13, 2),
+(13, 3),
+(13, 6),
+(13, 21),
+(15, 19),
+(19, 1),
+(19, 2),
+(19, 6),
+(19, 15),
+(21, 1),
+(21, 2),
+(21, 3),
+(21, 9),
+(21, 13);
