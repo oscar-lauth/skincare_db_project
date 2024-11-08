ae4be0f0263fe95001cd8277cc63e4c2f28868b0
Oscar Lauth <oscar.lauth@gmail.com>
2024-11-07 23:15:08 -0500

    Update functions testing

--- P4/functions_testing.sql
+++ P4/functions_testing.sql
@@ -1,4 +1,5 @@
-
+USE P3;
+GO
 -- Product with id 4 contains ingredient AHA which conflicts with ingredient Vitamin A which is in product id 25
 -- so should return 1 (yes conflicting)
 SELECT dbo.ProductsHaveConflictingIngredients(4,25) AS ProductsHaveConflictingIngredients;
