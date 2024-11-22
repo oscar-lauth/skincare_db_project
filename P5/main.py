80526e851d625ff6f1ccabf07c5b48c25854ea7d
Oscar Lauth <oscar.lauth@gmail.com>
2024-11-22 15:50:15 -0500

    autorun server on execute

--- P5/main.py
+++ P5/main.py
@@ -1,36 +1,18 @@
-from typing import Union
-import sqlalchemy as db
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import text
-
-engine = db.create_engine("mssql+pyodbc://sa:DB_password_alex@localhost:1433/P3?driver=ODBC+Driver+17+for+SQL+Server", echo=True)
-connection = engine.connect()
-metadata = db.MetaData()
-metadata.reflect(bind=connection)
-product_table = db.Table('Product', metadata)
-
-print(product_table.c.keys())
-
+from fastapi import FastAPI
+import uvicorn
+from queries import products
 app = FastAPI()
+
+# Register the product router
+app.include_router(products.router, prefix="/products", tags=["Products"])
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Word"}
+    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/products")
-def read_products():
-    stmt = db.select(product_table)
-    result = [row._mapping for row in connection.execute(stmt).all()]
-    return result
-
-@app.get("/products/{product_id}")
-def read_product(product_id: int):
-    stmt = db.select(product_table).where(product_table.c.productID == product_id)
-    result = connection.execute(stmt).all()
-    if len(result) == 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return result[0]._mapping
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        log_level="info",
+        reload=True
+    )
