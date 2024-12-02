db07ebba8b4cf0a8845346294c77ba8aeada9a7d
emmittjames <emmittjames1@gmail.com>
2024-12-02 02:03:35 -0500

    post function for making a new review

--- P5/queries/reviews.py
+++ P5/queries/reviews.py
@@ -37,3 +37,37 @@ def get_reviews_by_user(user_id: int, db: Engine = Depends(get_db)):
             detail = "This user has left no reviews"
         )
     return [row._mapping for row in result]
+
+@router.post("/")
+def create_review(review: dict, db: Engine = Depends(get_db)):
+    """
+    Insert a new review
+    """
+    if 'routineID' not in review or 'userID' not in review or 'reviewText' not in review or 'rating' not in review:
+        raise HTTPException(
+            status_code = 400,
+            detail = "Needs routineID and userID and reviewText and rating"
+        )
+    stmt1 = select(routine_table).where(routine_table.c.routineID == review['routineID'])
+    stmt2 = select(users_table).where(users_table.c.userID == review['userID'])
+    result1 = db.execute(stmt1).all()
+    result2 = db.execute(stmt2).all()
+    if not result1:
+        raise HTTPException(
+            status_code = 400,
+            detail = "RoutineID not found."
+        )
+    if not result2:
+        raise HTTPException(
+            status_code = 400,
+            detail = "UserID not found."
+        )
+    stmt = reviews_table.insert().values(
+        routineID = review['routineID'],
+        userID = review['userID'],
+        reviewText = review['reviewText'],
+        rating = review['rating']
+    )
+    db.execute(stmt)
+    db.commit()
+    return {"message": "Review created successfully"}
