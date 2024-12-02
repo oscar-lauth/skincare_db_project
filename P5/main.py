3b3b5bf7ae0e55f1083639375c463751d84f3806
emmittjames <emmittjames1@gmail.com>
2024-12-02 03:04:06 -0500

    added users to the router

--- P5/main.py
+++ P5/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
-from queries import products, routines, conflicts, ingredients, reviews, favorites
+from queries import products, routines, conflicts, ingredients, reviews, favorites, users
 app = FastAPI()
 
 app.include_router(products.router, prefix="/products", tags=["Products"])
@@ -9,6 +9,7 @@ app.include_router(conflicts.router, prefix="/conflicts", tags=["Conflicts"])
 app.include_router(ingredients.router, prefix="/ingredients", tags=["Ingredients"])
 app.include_router(reviews.router, prefix="/reviews", tags=["Reviews"])
 app.include_router(favorites.router, prefix="/favorites", tags=["Favorites"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 if __name__ == "__main__":
     uvicorn.run(
