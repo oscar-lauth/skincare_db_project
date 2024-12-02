37f7d773723cc4f48c09c7258abec65255e3af86
emmittjames <emmittjames1@gmail.com>
2024-12-01 23:57:21 -0500

    get reviews by user id

--- P5/queries/reviews.py
+++ P5/queries/reviews.py
@@ -10,16 +10,30 @@ reviews_table = Table('Reviews', metadata)
 routine_table = Table('Routine', metadata)
 users_table = Table('Users', metadata)
 
-@router.get("/{routine_id}")
+@router.get("/routine/{routine_id}")
 def get_reviews_by_routine(routine_id: int, db: Engine = Depends(get_db)):
     """
-    Get reviews by routine ID.
+    Get reviews by routine ID
     """
     stmt = select(reviews_table).where(reviews_table.c.routineID == routine_id)
     result = db.execute(stmt).all()
     if not result:
         raise HTTPException(
             status_code = 404,
-            detail = "No reviews for this routine."
+            detail = "This routine has no reviews"
+        )
+    return [row._mapping for row in result]
+
+@router.get("/user/{user_id}")
+def get_reviews_by_user(user_id: int, db: Engine = Depends(get_db)):
+    """
+    Get reviews by user ID
+    """
+    stmt = select(reviews_table).where(reviews_table.c.userID == user_id)
+    result = db.execute(stmt).all()
+    if not result:
+        raise HTTPException(
+            status_code = 404,
+            detail = "This user has left no reviews"
         )
     return [row._mapping for row in result]
